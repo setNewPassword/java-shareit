c0afc94a52998d2f68bec220b6558d01661326a4
Dee <dima.irqtsk@yandex.ru>
2023-07-19 18:06:52 +0800

    Remove unused imports #2

--- server/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -26,7 +26,6 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookingController.class)
